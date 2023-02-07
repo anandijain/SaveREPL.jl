c46d744dd68f3bfa31786572a9002f877739faff
anand jain <anandj@uchicago.edu>
2023-02-07 10:16:20 -0500

    small changes? idk

--- src/SaveREPL.jl
+++ src/SaveREPL.jl
@@ -22,14 +22,14 @@ Order: oldest command first. Ignores help and shell mode commands as well as
 """
 function saveREPL(filename::String, n::Int=10)
     open(filename, "w") do f
-        entries = history(n+1)
-        write(f, join((x->x.command).(entries[1:end-1]), "\n"))
+        entries = history(n + 1)
+        write(f, join((x -> x.command).(entries[1:end-1]), "\n"))
     end
     nothing
 end
 
 function saveREPL(io, n::Int=10)
-    entries = history(n+1)
+    entries = history(n + 1)
     for e in entries[1:end-1]
         write(io, e.command, "\n")
     end
@@ -43,7 +43,7 @@ Prints the `n` last executed Julia commands.
 Order defaults from old to new.
 """
 function printREPL(n::Int=10)
-    entries = history(n+1)
+    entries = history(n + 1)
     for e in entries[1:end-1]
         println(e.command)
     end
@@ -54,6 +54,13 @@ function copyREPL(n::Int=10)
     io = IOBuffer()
     saveREPL(io, n)
     clipboard(String(take!(io)))
+end
+
+macro cr(ex)
+    :(copyREPL($ex))
+end
+macro cr()
+    copyREPL(1)
 end
 
 """
@@ -67,31 +74,41 @@ function history(n::Int)
     i = 1
     N = length(h)
     c = 0
-    while i<=N && c<n
+    while i <= N && c < n
         line = h[i]
-        !contains(line, "saveREPL(") || (begin i+=1; continue; end)
-        !contains(line, "printREPL(") || (begin i+=1; continue; end)
+        !contains(line, "saveREPL(") || (
+            begin
+                i += 1
+                continue
+            end
+        )
+        !contains(line, "printREPL(") || (
+            begin
+                i += 1
+                continue
+            end
+        )
 
         cmdlines = String[]
         while startswith(line, "\t")
-            push!(cmdlines, replace(line, "\t"=>""; count=1))
-            i+=1
+            push!(cmdlines, replace(line, "\t" => ""; count=1))
+            i += 1
             line = h[i]
         end
         command = join(reverse(cmdlines), "\n")
 
         contains(line, "# mode:") || warn("wrong order: expected mode")
-        mode = replace(chomp(line), "# mode: "=>"")
+        mode = replace(chomp(line), "# mode: " => "")
 
-        i+=1
+        i += 1
         line = h[i]
         contains(line, "# time: ") || warn("wrong order: expected time")
-        time = replace(chomp(line), "# time: "=>"")
-        i+=1
+        time = replace(chomp(line), "# time: " => "")
+        i += 1
 
         contains(mode, "julia") || continue
         push!(entries, REPLEntry(time, mode, command))
-        c+=1
+        c += 1
     end
     return reverse(entries)
 end
